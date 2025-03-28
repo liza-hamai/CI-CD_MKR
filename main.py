549a203480589340508b5c309f01d1e09f50cdee
liza-hamai <elizaveta.hamai@knu.ua>
2025-03-28 10:52:42 +0200

    added compare_files function to main.py

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ def read_file(file_path):
     except FileNotFoundError:
         print(f"Файл {file_path} не знайдено.")
         return set()
+
+def compare_files(file1, file2):
+    """Порівнює вміст двох файлів і повертає спільні та унікальні рядки."""
+    lines1 = read_file(file1)
+    lines2 = read_file(file2)
+
+    common_lines = lines1 & lines2
+    different_lines = (lines1 | lines2) - common_lines
+
+    return common_lines, different_lines
